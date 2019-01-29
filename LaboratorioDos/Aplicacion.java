8f3dcb50a419a126da12e2967347286e6f4f39e5
Kevin Daniel <31493060+kevinbasto@users.noreply.github.com>
2019-01-29 09:15:56 -0600

    Actualización del método agregarNodo
    actualización menor del método para agregar nodos en el cual se empieza a implementar cambios menores al sistema

--- LaboratorioDos/Aplicacion.java
+++ LaboratorioDos/Aplicacion.java
@@ -15,10 +15,12 @@
 import java.util.Scanner;
 
 public class Aplicacion{
+	//cabezas de las listas
 	private static Nodo cabezaA;
 	private static Nodo cabezaB;
 	private static Nodo cabezaC;
 	private static Nodo CabezaD;
+	//contadores de los nodos
 	private static int  nodosA;
 	private static int  nodosB;
 	private static int  nodosC;
@@ -49,16 +51,16 @@ public class Aplicacion{
 
 			switch(selector){
 				case 1:
-					agregaNodo();
+					agregaNodo(scanner, cabezaA);
 					break;
 				case 2:
-					agregaNodo();
+					agregaNodo(scanner, cabezaB);
 					break;
 				case 3:
-					imprimeNodo();
+					imprimeNodo(cabezaA);
 					break;
 				case 4:
-					imprimeNodo();
+					imprimeNodo(cabezaB);
 					break;
 				case 5:
 					generaSuma();
@@ -74,11 +76,27 @@ public class Aplicacion{
 		}while(selector != 0);
 	}
 
-	private static void agregaNodo(Scanner scanner){
+	/*
+	*	agregaNodo
+	*   ----------------------------------------------------------
+	*	este método agrega nodos a la lista de modo que los va acomodando
+	*	conforme al exponente
+	*/
+	private static void agregaNodo(Scanner scanner, Nodo cabeza){
 		int exponente;
+		int coeficiente;
+		char literal;
+
+		clearScreen();
+		System.out.println("Bienvenido al sistema para agregar nodos a la lista\n");
+		System.out.println("---------------------------------------------------\n");
+		System.out.println("introduzca el exponente: ");
+		exponente = scanner.nextInt();
+		clearScreen();
+		System.out.println("");
 	}
 
-	private static void imprimeNodo(){
+	private static void imprimeNodo(Nodo cabeza){
 
 	}
 
