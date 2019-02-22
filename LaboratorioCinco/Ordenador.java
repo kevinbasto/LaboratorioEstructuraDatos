b386c7286b0ba3450bb3aac24ad44f56df13d553
kevinbasto <bastokevindaniel@gmail.com>
2019-02-22 08:14:49 -0600

    creación de la clase Ordenador

---
+++ LaboratorioCinco/Ordenador.java
@@ -0,0 +1,12 @@
+/*
+*	Clase: Ordenador
+*--------------------------------------------------------------
+*	Esta es la única clase del proyecto y tiene la función de
+* de almacenar un monton de información aleatoria y ordenarla
+* por los múltiples métodos de ordenamiento.
+*
+*/
+
+public class Ordenador{
+	
+}
